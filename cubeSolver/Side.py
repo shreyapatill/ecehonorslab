bc374cd0b2f060d49bec4e2e59cb60bedbe1e144
BrainyPanda <alex.my.zhang@outlook.com>
2020-10-04 10:38:06 -0500

    Cube solver V2, using valid moves

---
+++ cubeSolver/Side.py
@@ -0,0 +1,20 @@
+#Entire code copied from https://github.com/MineLT/CubeSolver/blob/master/solver%20with%20software/Arete.py
+
+class Arete():
+
+    def __init__(self, c1, c2):
+        self.couleur = [c1, c2]
+
+    def getCouleur(self, i):
+        return self.couleur[i-1]
+
+    def setCouleur(self, i, c):
+        self.couleur[i-1] = c
+
+    def contient(self, c1, c2):
+
+        if(self.couleur[0] == c1 and self.couleur[1] == c2 or
+           self.couleur[0] == c2 and self.couleur[1] == c1):
+            return True
+
+        return False
